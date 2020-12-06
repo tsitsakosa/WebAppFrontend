e741269bddb708e99c65975352aa5a257b4475a6
tsitsakosa <60984763+tsitsakosa@users.noreply.github.com>
2020-12-06 23:07:15 +0100

    Update deploy script

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-import execa from "execa";
-import fs from "fs";
+const execa = require("execa");
+const fs = require("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
